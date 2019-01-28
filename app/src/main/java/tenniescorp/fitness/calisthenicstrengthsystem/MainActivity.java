099556635826c485ffc2fe0d99b06c17d1b32da3
jtennies10 <jdtennies10@gmail.com>
2019-01-28 14:12:44 -0500

    Minor xml clean up

--- app/src/main/java/tenniescorp/fitness/calisthenicstrengthsystem/MainActivity.java
+++ app/src/main/java/tenniescorp/fitness/calisthenicstrengthsystem/MainActivity.java
@@ -3,7 +3,6 @@ package tenniescorp.fitness.calisthenicstrengthsystem;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
