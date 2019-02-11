45645e408d95cef9b2b84d9d1d22ef3a4837902f
jtennies10 <jdtennies10@gmail.com>
2019-02-11 14:05:37 -0500

    Started writing code to predict future weight

--- app/src/main/java/tenniescorp/fitness/calisthenicstrengthsystem/WeightTrackerActivity.java
+++ app/src/main/java/tenniescorp/fitness/calisthenicstrengthsystem/WeightTrackerActivity.java
@@ -16,10 +16,12 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 
 import java.text.SimpleDateFormat;
 import java.time.Instant;
@@ -48,6 +50,8 @@ public class WeightTrackerActivity extends AppCompatActivity {
 
         CSSRoomDatabase db = CSSRoomDatabase.getDatabase(getApplicationContext());
         userWeights = db.userWeightDao().getCurrentUserWeights(userId);
+
+        userWeights.add(calculateFutureUserWeight());
 
         recyclerView = findViewById(R.id.recyclerview);
 
@@ -102,6 +106,11 @@ public class WeightTrackerActivity extends AppCompatActivity {
         }
         int weightInPounds = Integer.parseInt(weightView.getText().toString());
 
+        if(weightInPounds <= 0) {
+            Toast.makeText(getApplicationContext(), "Invalid weight", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         UserWeight newUserWeight = new UserWeight(userId, weightInPounds, new Date().getTime());
 
         CSSRoomDatabase db = CSSRoomDatabase.getDatabase(getApplicationContext());
@@ -121,6 +130,8 @@ public class WeightTrackerActivity extends AppCompatActivity {
         //Hide keyboard code found at https://stackoverflow.com/questions/4165414/how-to-hide-soft-keyboard-on-android-after-clicking-outside-edittext
         InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+
+        populateChart();
     }
 
 //    private String getDateAsString() {
@@ -145,27 +156,73 @@ public class WeightTrackerActivity extends AppCompatActivity {
             userWeightEntries.add(new Entry(current.getWeightDateAsLong(), current.getWeightInPounds()));
         }
 
-
+//        String[] months = getXAxisMonths();
 
         LineDataSet dataSet = new LineDataSet(userWeightEntries, "User Weights");
         dataSet.setColor(Color.RED);
         dataSet.setHighLightColor(Color.RED);
-        //dataSet.setDrawFilled(true);
-
-
-//        ArrayList<String> labels = new ArrayList<>();
-//        labels.add("January");
-//        labels.add("February");
-//        labels.add("March");
-//        labels.add("April");
 
         LineData data = new LineData(dataSet);
         lineChart.setData(data);
-
-        XAxis xAxis = lineChart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-
-
+        lineChart.getXAxis().setEnabled(false);
 
     }
+
+    private UserWeight calculateFutureUserWeight() {
+        UserWeight firstWeightRecord = userWeights.get(0);
+        UserWeight lastWeightRecord = userWeights.get(userWeights.size()-1);
+
+        long minWeightDate = firstWeightRecord.getWeightDateAsLong();
+        long maxWeightDate = lastWeightRecord.getWeightDateAsLong();
+
+        long datesRange = maxWeightDate - minWeightDate;
+        long predictedWeightDate = maxWeightDate + (datesRange / 2);
+
+        int minWeightLbsIndex = findMinWeightIndex();
+        int maxWeightLbsIndex = findMaxWeightIndex();
+
+        int weightLbsRange = userWeights.get(minWeightLbsIndex).getWeightInPounds() -
+                userWeights.get(maxWeightLbsIndex).getWeightInPounds();
+        if(weightLbsRange < 0) weightLbsRange *= -1;
+
+        //todo:predict weight based on range and whether min or max comes first
+
+
+        int predictedWeightLbs;
+
+
+        //todo:return calculated weight
+        return null;
+
+    }
+
+    private int findMinWeightIndex() {
+        int currentMinWeight = Integer.MAX_VALUE;
+        int currentMinIndex = 0;
+
+        for(int i = 0; i < userWeights.size(); i++) {
+            if(userWeights.get(i).getWeightInPounds() < currentMinWeight) {
+                currentMinWeight = userWeights.get(i).getWeightInPounds();
+                currentMinIndex = i;
+            }
+        }
+
+        return currentMinIndex;
+    }
+
+    private int findMaxWeightIndex() {
+        int currentMaxWeight = -1;
+        int currentMaxIndex = 0;
+
+        for(int i = 0; i < userWeights.size(); i++) {
+            if(userWeights.get(i).getWeightInPounds() > currentMaxWeight) {
+                currentMaxWeight = userWeights.get(i).getWeightInPounds();
+                currentMaxIndex = i;
+            }
+        }
+
+        return currentMaxIndex;
+    }
+
+
 }
